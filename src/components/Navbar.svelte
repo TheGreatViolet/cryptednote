718054783d51dc45697a1e57b8c0be57d8cd52b6
Violet Hayes <ampc11234@gmail.com>
2022-08-20 17:53:05 -0700

    Created home page

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <div
-  class="h-16 p-4 align-middle
+  class="h-16 w-full p-4 align-middle
   bg-primary border-b-2 border-secondary border-opacity-20"
 >
-  <NavbarButton route="/" text="Home" />
+  <NavbarButton route="/" text="CryptedNote" />
 </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,10 +3,14 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		colors: {
-      primary: "#1B2444",
+      primary: "#11142D",
       secondary: "#C1CCCC",
       accent: "#195B7D",
+			lightAccent: "#227DAC",
     },
+		fontFamily: {
+			body: ['Open Sans', 'sans-serif'],
+		},
 		extend: {},
 	},
 	plugins: [],
