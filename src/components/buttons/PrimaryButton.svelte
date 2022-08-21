72246c36fdcf39e18d8a7e27ba7c0130966589cd
Violet Hayes <ampc11234@gmail.com>
2022-08-20 19:17:09 -0700

    Created signup page

---
+++ src/components/buttons/PrimaryButton.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let onClick: () => void = () => {};
+</script>
+
+<button
+  on:click={onClick}
+  class="py-2 px-4 rounded-md bg-accent text-secondary hover:bg-lightAccent transition-colors duration-200 ease-in-out"
+>
+  <slot />
+</button>
